1449d0f4e525dcd7ac74d93c5df0527f82baf48c
wuyao19952016 <2438626764@qq.com>
2019-02-18 14:18:29 +0800

    更新飞机游戏，增加设计功能，解决闪烁问题

--- Game/planeGame.c
+++ Game/planeGame.c
@@ -2,52 +2,107 @@
 #include<conio.h>
 #include<stdlib.h>
 #include<windows.h>
+// 全局变量
+int post_x,post_y;//飞机位置
+int high,width;//游戏画面尺寸
+int bullet_x,bullet_y;
+
+
+void HideCursor();
+void startup();
+void show();
+void updateWithoutInput();
+void updateWithInput();
+void gotoxy(int x,int y);
+
+int main()
+{
+	HideCursor();
+	startup();
+	while(1)
+	{
+		show();
+		updateWithoutInput();
+		updateWithInput();
+	}
+	
+    return 0;
+}
+
+void startup()
+{
+	high=20;
+	width=30;
+	
+	post_x=high/2;
+	post_y=width/2;
+	bullet_x=0;
+	bullet_y=post_y;
+}
+
 void HideCursor()
 {
 	CONSOLE_CURSOR_INFO cursor_info={1,0};
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&cursor_info);
 }
-int main()
+
+void show()
 {
-	HideCursor();
-	int x=5,y=10;
-	char input;
-	while(1)
+	gotoxy(0,0);
+	for(int i=1;i<=high;i++)
 	{
-		system("cls");
-		
-		for(int i=0;i<x;i++)
+		for(int j=1;j<=width;j++)
 		{
-			printf("\n");
+			if(i==post_x&&j==post_y)
+			{
+				printf("*");
+			}else if(i==bullet_x&&j==bullet_y)
+			{
+				printf("|");
+			}else 
+			{
+				printf(" ");
+			}
 		}
-		
-		for(int j=0;j<y;j++)
-		{
-			printf(" ");
-		}
-		printf("*");
 		printf("\n");
-		
-		if(kbhit())
+	}
+}
+void updateWithoutInput()
+{
+	if(bullet_x>-1)
+	{
+		bullet_x--;
+	}
+}
+
+void updateWithInput()
+{
+	char input;
+	if(kbhit())
+	{
+		input=getch();
+		if(input=='a')
+			post_y--;
+		if(input=='d')
+			post_y++;
+		if(input=='w')
+			post_x--;
+		if(input=='s')
+			post_x++;
+		if(input==' ')
 		{
-			input=getch();
-			if(input=='a')
-			{
-				y--;
-			}
-			if(input=='d')
-			{
-				y++;
-			}
-			if(input=='w')
-			{
-				x--;
-			}
-			if(input=='s')
-			{
-				x++;
-			}
+			bullet_x=post_x-1;
+			bullet_y=post_y;
 		}
 	}
-    return 0;
 }
+void gotoxy(int x,int y)
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.X=x;
+	pos.Y=y;
+	SetConsoleCursorPosition(handle,pos);
+}
+
+
