923d0855ebd484bb4c9b9a71a589cd76571d9627
wuyao19952016 <2438626764@qq.com>
2019-05-06 22:18:29 +0800

    报时

---
+++ C++1/03-变量研究所/04报时.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main()
+{
+    int h,m,s;
+    scanf("%d:%d:%d",&h,&m,&s);
+    cout<<"现在时间是"<<h<<"点"<<m<<"分"<<s<<"秒";
+
+    return 0;
+}
